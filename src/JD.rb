420a73985ea3ff0ddc371af795d9917ac0d492b5
Olli Wilkman <olli.wilkman@iki.fi>
2016-01-25 00:00:06 +0200

    JD seems to work now, still needs time

--- src/JD.rb
+++ src/JD.rb
@@ -1,24 +1,20 @@
 
 class JD
   include Cinch::Plugin
+  require "date"
   
-  match /JD +([0-9]+(\.[0-9]+)?[\W]*)/, method: :JDtoDate
-  match /JD ([0-9]+\-[0-9]+\-[0-9]+)/, method: :DatetoJD
+  # JD to date regex
+  match /JD +([0-9]+(\.[0-9]+)?[\W]*$)/, method: :JDtoDate
   
-  def DatetoJD(m, date_string)
-    my_match = /([0-9]+)[\.\-]([0-9]+)[\.\-]([0-9]+)/.match(date_string)
-    puts my_math.captures
-    day = my_match[1].to_i
-    month = my_match[2].to_i
-    year = my_match[3].to_i
-    a = ((14-month)/12.0).floor
-    y = year + 4800.0 - a
-    m = month + 12*a - 3.0
-    j = day + ((153*m+2)/5).floor + 365*y + (y/4).floor - (y/100).floor + (y/400).floor - 32045
-  end
+  # Date to JD regex
+  match /JD ([0-9]+[\-\.][0-9]+[\-\.][0-9]+)/, method: :DatetoJD
   
-  def JDtoDate(m, jd_string)
-    s = m.user.nick
+  match /JD[\W]*$/, method: :JDNow
+    
+  # Convert JD to a date
+  def JDtoDate(event, jd_string)
+    user = event.user.nick
+    puts jd_string
     j = jd_string.to_f - 0.25
     f = j + 1401 + ((4*j+274277).div(146097)*3).div(4) - 38
     e = 4*f + 3
@@ -27,6 +23,28 @@ class JD
     day = (h % 153).div(5) + 1
     month = ((h.div(153) + 2) % 12) + 1
     year = e.div(1461) - 4716 + (14-month).div(12)
-    m.channel.msg("#{s}: #{day}.#{month}.#{year}")
+    event.channel.msg("#{user}: #{day}.#{month}.#{year}")
   end
+  
+  # Convert a date to JD
+  def DatetoJD(event, date_string)
+    my_match = /([0-9]+)[\.\-]([0-9]+)[\.\-]([0-9]+)/.match(date_string)
+    puts my_match.captures
+    day = my_match[1].to_i
+    month = my_match[2].to_i
+    year = my_match[3].to_i
+    a = ((14-month)/12.0).floor
+    y = year + 4800.0 - a
+    m = month + 12*a - 3.0
+    j = day + ((153*m+2)/5).floor + 365*y + (y/4).floor - (y/100).floor + (y/400).floor - 32045
+    user = event.user.nick
+    event.channel.msg("#{user}: #{j}")
+  end
+  
+  def JDNow(event)
+    now = DateTime.now
+    time_string = now.strftime("%d-%m-%Y")
+    DatetoJD(event, time_string)
+  end
+  
 end
