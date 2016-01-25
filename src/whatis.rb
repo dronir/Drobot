c19fdc9c34017108900b0886fde7aad55361a326
Olli Wilkman <olli.wilkman@iki.fi>
2016-01-26 00:54:36 +0200

    Rudimentary working database implementation

--- src/whatis.rb
+++ src/whatis.rb
@@ -1,13 +1,77 @@
 class WhatIs
   include Cinch::Plugin
+  require 'date'
+  require 'sqlite3'
 
-  set :prefix, lambda{ |m| Regexp.new("^" + Regexp.escape(m.bot.nick + ": " ))}
+  def initialize(*)
+    super
+    begin
+      @db = SQLite3::Database.open bot.config.whatis_db_path
+      @db.results_as_hash = true
+      @status = true
+    rescue SQLite3::Exception => e
+      puts "Problem connecting to WhatIs database!"
+      puts e
+      @status = false
+      @db.close if @db
+    end
+  end
 
-  match /w(hatis|tf)? ([^S]+)/, method: :whatis_query
+  set :prefix, /^/
+
+  # Regex for whatis query
+  match /(w|!whatis)?\s+([^S]+)/, method: :whatis_query
+  
+  # Regex for definition
+  match /!define ([\S]+)\s+(.+)/, method: :definition
+  
+  # Regex for whodef
+  match /!whodef\s+(\w+)\s+([0-9]+)/, method: :whodef
+  
   
   def whatis_query(event, sufx, query)
-    user = event.user.nick
-    event.channel.msg("#{user}: I don't yet know what #{(query)} is.")
+    synchronize(:define_db_access) do
+      user = event.user.nick
+      begin
+        stm = @db.prepare "SELECT * FROM entries WHERE key = ?"
+        stm.bind_param 1, query
+        rs = stm.execute
+        i = 0
+        rs.each do |row|
+          i += 1
+          key = row["key"]
+          value = row["definition"]
+          event.channel.msg("#{key} (#{i}): #{value}")
+          sleep 0.5  
+        end
+      rescue SQLite3::Exception => e
+        event.channel.msg("Something went wrong.")
+      end
+    end
+  end
+  
+
+  def definition(event, key, value)
+    synchronize(:define_db_access) do
+      user = event.user.nick
+      timestamp = DateTime.now.strftime("%d-%m-%Y %H:%M:%S.%3N")
+      begin
+        stm = @db.prepare "insert into entries (key, definition, definer) values (?, ?, ?)"
+        stm.bind_param 1, key
+        stm.bind_param 2, value
+        stm.bind_param 3, user
+        stm.execute
+      
+        event.channel.msg("#{user}: Definition added.")
+      rescue SQLite3::Exception => e
+        event.channel.msg("#{user}: Something went wrong.")
+        puts "Something went wrong: #{user}: #{key} = #{value}"
+        puts e
+      end
+    end    
+  end
+  
+  def whodef(event, key, index)
   end
   
 end
