ef02feaba77184b206a04a61175f5d75af28691d
Olli Wilkman <olli.wilkman@iki.fi>
2016-01-30 02:11:25 +0200

    Read config from a TOML file

--- src/Drobot.rb
+++ src/Drobot.rb
@@ -1,20 +1,21 @@
 # -*- coding: utf-8 -*-
 require "cinch"
+require "toml"
 
 require_relative "joinpart"
 require_relative "JD"
 require_relative "whatis"
 
-SERVER = ARGV[0]
+CONFIG = TOML.load_file("config.toml")
 
 # Create the bot
 drobot = Cinch::Bot.new do
   configure do |c|
-    c.nick = "Drobot"
-    c.server = SERVER
-    c.channels = ["#asdfoj"]
+    c.nick = CONFIG["nick"]
+    c.server = CONFIG["server"]
+    c.channels = CONFIG["channels"]
     c.plugins.plugins = [JoinPart, WhatIs, JD]
-    c.whatis_db_path = "test.db"
+    c.whatis_db_path = CONFIG["WhatIs"]["db_path"]
   end
 end
 
